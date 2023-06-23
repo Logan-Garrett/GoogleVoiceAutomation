b597562ebb30c730eae7d41e447509b89d4af7e9
Logan <ninetailsoftware@gmail.com>
2023-06-22 21:38:05 -0400

    Ordering.

--- AutomatedMessangerSelenium/AutomatedMessanger/Program.cs
+++ AutomatedMessangerSelenium/AutomatedMessanger/Program.cs
@@ -1,31 +1,21 @@
 ﻿using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
-using System;
-using System.Threading;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, RPM!");
 
 var service = @"E:\git\localhost-data";
-
 var options = new ChromeOptions();
+
 options.AddArguments("--no-sandbox");
 options.AddArguments("--disable-dev-shm-usage");
 options.AddArguments("--remote-debugging-port=9222");
+options.AddArgument("headless");
 options.DebuggerAddress = "127.0.0.1:9222";
 
-//options.AddArgument("headless");
 //chrome.exe --remote-debugging-port=9222 --user-data-dir="E:\git\localhost-data"
 IWebDriver driver = new ChromeDriver(service, options);
 driver.Navigate().GoToUrl("https://voice.google.com/u/3/messages");
-Thread.Sleep(2000);
-
-// Not Needed as of now
-//driver.FindElement(By.XPath("//*[@class='signUpLink']")).Click();
-//Thread.Sleep(2000);
-//driver.FindElement(By.XPath("//*[@aria-label='Email or phone']")).SendKeys("");
-//driver.FindElement(By.XPath("//*[@class='VfPpkd-RLmnJb']")).Click();
-
-//md-ink-ripple layout-align-start-center layout-row gmat-body-2 gvThreadItem-root
+Thread.Sleep(1000);
 
 var ContactList = driver.FindElements(By.XPath("//div[@class='md-ink-ripple layout-align-start-center layout-row gmat-body-2 gvThreadItem-root']"));
 
